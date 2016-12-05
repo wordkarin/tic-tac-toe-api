a0af1e9f63cc6192442678d7e2bb0395085c30a1
Charles Ellis <hamled@hamled.name>
2016-12-05 12:05:57 -0800

    GamesController#index action base tests

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,9 +1,39 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
+  def setup
+    @controller = GamesController.new
+  end
+
   class General < GamesControllerTest
     test "GamesController should exist" do
       assert GamesController
     end
+  end
+
+  class Actions < GamesControllerTest
+    def self.base_tests_for_get(action)
+      test "should accept GET requests" do
+        assert get action
+      end
+
+      test "should return success" do
+        get action
+        assert_response :success
+      end
+
+      test "should return JSON" do
+        get action
+        assert_equal Mime[:json].to_s, response.content_type
+      end
+    end
+
+    def parsed_body
+      JSON.parse(response.body)
+    end
+  end
+
+  class IndexAction < Actions
+    base_tests_for_get(:index)
   end
 end
