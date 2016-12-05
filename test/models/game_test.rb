5eea78f4f104e9c590ec1000e8c2917d63cc46a2
Charles Ellis <hamled@hamled.name>
2016-12-05 11:30:15 -0800

    Refactor Game model tests
    This change just moves the one test located directly inside GameTest to
    be within a subclass of it instead. Because all of the subclasses
    inherit from GameTest each one was duplicating that test, which is not
    desirable.

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,13 +1,15 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  test "can create a Game" do
-    game = create(:game)
+  class General < GameTest
+    test "can create a Game" do
+      game = create(:game)
 
-    assert_not_nil game
-    assert_kind_of Hash, game.data
-    refute_empty game.data
-    assert game.persisted?
+      assert_not_nil game
+      assert_kind_of Hash, game.data
+      refute_empty game.data
+      assert game.persisted?
+    end
   end
 
   class Validations < GameTest
