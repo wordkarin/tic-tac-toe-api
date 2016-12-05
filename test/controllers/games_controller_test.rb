6450a7e730fa1c5893467e676e4a633f00ebcf64
Charles Ellis <hamled@hamled.name>
2016-12-05 11:41:11 -0800

    GamesController (empty)

---
+++ test/controllers/games_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class GamesControllerTest < ActionController::TestCase
+  class General < GamesControllerTest
+    test "GamesController should exist" do
+      assert GamesController
+    end
+  end
+end
