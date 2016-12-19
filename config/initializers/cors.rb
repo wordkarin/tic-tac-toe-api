f33ebd92b0f85f476ddd08ad743a9c3b887770ca
Charles Ellis <hamled@hamled.name>
2016-12-19 11:51:53 -0800

    Allow cross-origin requests from anywhere

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8081'
+    origins '*'
 
     resource '/api/v1/games/*',
       headers: :any,
